aa4501069b1f2d806596c7d073eea37a1e707c96
Anu <anu.khatra@gmail.com>
2020-08-27 17:09:50 +0200

    connection commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Socket-chatt-with-Web-API-integration-
 Socket chatt with Web API integration 
+
+
+Command used to install :
+
+npm init for package.json
+
+npm install express
+
+npm install socket.io
+
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http,{pingTimeout:25000})
+const port = 3000 ;
+
+app.use(express.static('public'))
+
+function getConnection(socket){
+    console.log("Connection done" + socket.id)
+
+}
+io.on("connection", getConnection)
+
+http.listen(port,() => console.log('Connecting to port'+port))
