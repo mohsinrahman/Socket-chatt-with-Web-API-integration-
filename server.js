1fe701c64acce4d38bde6f515373f6358de7a478
Anu <anu.khatra@gmail.com>
2020-08-27 17:45:35 +0200

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http,{pingTimeout:25000})
-const port = 3000 ;
+const port = 3000 || process.env.PORT ;
 
 app.use(express.static('public'))
 
@@ -12,4 +12,4 @@ function getConnection(socket){
 }
 io.on("connection", getConnection)
 
-http.listen(port,() => console.log('Connecting to port'+port))
+http.listen(port,() => console.log('Connecting to port'+ port))
