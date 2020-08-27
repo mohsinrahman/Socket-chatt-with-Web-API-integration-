10e35fec53e6dd0abe8341e41c9713598a9c0770
Anu <anu.khatra@gmail.com>
2020-08-27 17:48:16 +0200

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http,{pingTimeout:25000})
-const port = 3000 ;
+const port = 3000 || process.env.PORT ;
 
 app.use(express.static('public'))
 
