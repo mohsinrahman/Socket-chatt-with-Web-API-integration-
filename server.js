21d37aca2c2eddb7bb3ffc0cba39c4d8eef9471f
Bara Abdullatif <bara.abd@yahoo.com>
2020-08-30 23:02:59 +0200

    Get callback message, message has delivered, And block messages that contain profanity

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 const http = require('http').Server(app)
+const Filter = require('bad-words')
+
 const io = require('socket.io')(http,{pingTimeout:25000})
-const port = 3000 || process.env.PORT ;
+const port = 3000 || process.env.PORT 
 
 app.use(express.static('public'))
 
@@ -16,12 +18,20 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
     })
 
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', () => {
