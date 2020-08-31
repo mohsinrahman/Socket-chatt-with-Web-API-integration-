fb506e8f5b83dae340ffa8c0750a9a3625b69945
Bara Abdullatif <bara.abd@yahoo.com>
2020-08-31 22:26:03 +0200

    Fix command /, show one command ( send location )

--- public/client.js
+++ public/client.js
@@ -7,11 +7,27 @@ const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
 
 socket.on('message', (message) => {
     console.log(message)
+    if (message.text == '/') {
+        $sendLocationButton.setAttribute("style", "display: block;");
+    }
     const html = Mustache.render(messageTemplate, {
-        message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const http = require('http').Server(app)
-const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+
 
 const io = require('socket.io')(http,{pingTimeout:25000})
 const port = 3000 || process.env.PORT 
@@ -15,27 +16,29 @@ function getConnection(socket){
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
     socket.on('sendMessage', (message, callback) => {
-        const filter = new Filter()
-
-        if (filter.isProfane(message)) {
-            return callback('Profanity is not allowed!')
+        const filter = '//'
+        
+        if (message == filter) {
+            return callback('Command prompt')
         }
 
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback()
+
+
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
